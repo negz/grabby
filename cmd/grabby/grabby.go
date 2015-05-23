a5d8ec91e16bf2338f83cc9abb67b07d8469a231
Nic Cope <negz@spotify.com>
2015-05-23 00:18:07 -0400

    Move package nnntp to nntp, and use it.

--- cmd/grabby/grabby.go
+++ cmd/grabby/grabby.go
@@ -8,26 +8,27 @@ import (
 	"time"
 
 	"github.com/negz/grabby/decode/yenc"
-	"github.com/negz/grabby/nnntp"
+	"github.com/negz/grabby/nntp"
 	"github.com/negz/grabby/nzb"
 	"github.com/negz/grabby/util"
+	"github.com/pivotal-golang/bytefmt"
 	"gopkg.in/alecthomas/kingpin.v1"
-)
-
-var (
-	server      = kingpin.Flag("server", "Usenet server hostname").Short('s').Required().String()
-	username    = kingpin.Flag("username", "Usenet server username").Short('u').Required().String()
-	connections = kingpin.Flag("connections", "Usenet max connections").Short('c').Required().Int()
-	passfile    = kingpin.Flag("passfile", "Usenet server password file").Short('p').Required().ExistingFile()
-	nzbfile     = kingpin.Arg("nzb", "NZB file to download").Required().ExistingFile()
-	outdir      = kingpin.Arg("outdir", "File to download to").Required().ExistingDir()
 )
 
 func outFile(path, nzb, subject string, segment int) string {
 	return filepath.Join(path, fmt.Sprintf("%v.%v.%v", nzb, util.HashString(subject), segment))
 }
 
+// This is mostly a horrible test harness for the moment.
 func main() {
+	var (
+		server      = kingpin.Flag("server", "Usenet server hostname").Short('s').Required().String()
+		username    = kingpin.Flag("username", "Usenet server username").Short('u').Required().String()
+		connections = kingpin.Flag("connections", "Usenet max connections").Short('c').Required().Int()
+		passfile    = kingpin.Flag("passfile", "Usenet server password file").Short('p').Required().ExistingFile()
+		nzbfile     = kingpin.Arg("nzb", "NZB file to download").Required().ExistingFile()
+		outdir      = kingpin.Arg("outdir", "File to download to").Required().ExistingDir()
+	)
 	kingpin.Parse()
 
 	password, err := util.PasswordFromFile(*passfile)
@@ -36,7 +37,7 @@ func main() {
 	}
 
 	// Connect to server
-	s := nnntp.NewServer(*server, 119, false, *username, password, *connections)
+	s := nntp.NewServer(*server, 119, false, *username, password, *connections)
 
 	// Parse NZB file
 	n, err := nzb.NewFromFile(*nzbfile)
@@ -45,7 +46,7 @@ func main() {
 	}
 
 	// Make a slice of things to grab.
-	var grab []*nnntp.ArticleRequest
+	var requests []*nntp.ArticleRequest
 	for _, file := range n.Files {
 		for _, group := range file.Groups {
 			for _, segment := range file.Segments {
@@ -55,44 +56,49 @@ func main() {
 					log.Fatalf("Unable to create output file %v: %v", fp, err)
 				}
 				defer of.Close()
-				request := &nnntp.ArticleRequest{Group: group, ID: segment.ArticleID, WriteTo: yenc.NewDecoder(of)}
-				grab = append(grab, request)
+				request := &nntp.ArticleRequest{Group: group, ID: segment.ArticleID, WriteTo: yenc.NewDecoder(of)}
+				requests = append(requests, request)
 			}
 		}
 	}
+	log.Printf("Will send %v requests", len(requests))
 
-	// Send requests.
-	// Create grabby channels
-	req := make(chan *nnntp.ArticleRequest, len(grab))
-	resp := make(chan *nnntp.ArticleResponse, len(grab))
+	// Create grabbers
+	req := make(chan *nntp.ArticleRequest, len(requests))
+	resp := make(chan *nntp.ArticleResponse, len(requests))
 	for i := 0; i < s.MaxSessions; i++ {
-		sn, err := nnntp.NewSession(s)
+		sn, err := nntp.NewSession(s, nntp.Dial)
 		if err != nil {
-			log.Fatalf("Failed to create new session for %v: %v", s, err)
+			log.Printf("Failed to create new session for %v: %v", s, err)
+			continue
 		}
-		// TODO(negz): This should probably happen if/when req is closed?
-		defer sn.Close()
-		go sn.Grabby(req, resp)
-	}
-	for _, request := range grab {
-		req <- request
+		defer sn.Quit()
+		go sn.Grab(req, resp)
 	}
 
-	// Wait for the grabbening.
-	for {
+	// Do the grabbening.
+	startTime := time.Now()
+	for _, r := range requests {
+		req <- r
+	}
+
+	var responses []*nntp.ArticleResponse
+	var tb int64 = 0
+	for len(requests) > len(responses) {
 		select {
-		case response := <-resp:
-			if response.Error != nil {
-				log.Printf("Error getting article %v: %v", response.ID, response.Error)
+		case r := <-resp:
+			if r.Error != nil {
+				log.Printf("Error getting article %v: %v", r.ID, r.Error)
 			}
-			log.Printf("Wrote %v bytes for article %v", response.Bytes, response.ID)
-		// TODO(negz): This doesn't seem to work.
-		case <-time.After(time.Second * 30):
-			log.Fatalln("Suspiciously quiet. Are we done?")
-		default:
-			log.Println("No responses to handle.")
-			time.Sleep(time.Second * 3)
+			log.Printf("Wrote %v for article %v", bytefmt.ByteSize(uint64(r.Bytes)), r.ID)
+			responses = append(responses, r)
+			tb += r.Bytes
 		}
 	}
 
+	d := time.Since(startTime)
+	rate := float64(tb) / d.Seconds()
+	log.Printf(
+		"Downloaded %v in %v (%v/s)",
+		bytefmt.ByteSize(uint64(tb)), d, bytefmt.ByteSize(uint64(rate)))
 }
--- nntp/nntp.go
+++ nntp/nntp.go
@@ -1,7 +1,6 @@
 /*
-Package nntp implements functions for interfacing with an NNTP server.
-It is designed to handle multiple sessions, and has a focus on downloading
-binary files.
+Package nntp is a light wrapper around github.com/willglynn/nntp.
+It provides convenience for multi-connection binary downloads.
 */
 package nntp
 
@@ -9,84 +8,88 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
-	"sync"
 
 	"github.com/negz/grabby/util"
 	"github.com/willglynn/nntp"
 )
 
-// A SessionError records an error handling a server's sessions.
-type SessionError string
-
-func (err SessionError) Error() string {
-	return string(err)
+// conner is an interface that wraps the functionality of nntp.Conn().
+type conner interface {
+	Authenticate(u, p string) error
+	EnableCompression() error
+	Group(g string) (*nntp.Group, error)
+	Body(id string) (io.Reader, error)
+	Quit() error
 }
 
-// A Session represents a single connection to a usenet server.
+// A Server represents an NNTP server, which may have many connections
+type Server struct {
+	Hostname    string
+	Port        int
+	TLS         bool
+	TLSConfig   *tls.Config
+	Username    string
+	password    string
+	MaxSessions int
+}
+
+// String returns the server as a hostname:port string.
+func (s *Server) String() string {
+	return fmt.Sprintf("%v:%v", s.Hostname, s.Port)
+}
+
+// NewServer creates and a initialises a new Server.
+func NewServer(hostname string, port int, useTLS bool, username, password string, maxSessions int) *Server {
+	return &Server{
+		Hostname:    hostname,
+		Port:        port,
+		TLS:         useTLS,
+		TLSConfig:   &tls.Config{InsecureSkipVerify: true},
+		Username:    username,
+		password:    password,
+		MaxSessions: maxSessions,
+	}
+}
+
+// A dialer dials a server and returns a conner.
+type dialer func(*Server) (conner, error)
+
+// Dial dials the supplied *Server, returning a connection.
+func Dial(s *Server) (conner, error) {
+	if s.TLS {
+		return nntp.DialTLS("tcp", fmt.Sprint(s), s.TLSConfig)
+	}
+	return nntp.Dial("tcp", fmt.Sprint(s))
+}
+
+// A Session represents a single connection to a *Server.
 type Session struct {
 	Server       *Server
 	CurrentGroup string
 	Compressed   bool
-	Busy         bool
-	Connected    bool
-	conn         *nntp.Conn
+	c            conner
 }
 
-// dial connects, authenticates, and attempts to enable compression.
-func (s *Session) dial() error {
-	if s.Connected {
-		return SessionError("session is already connected")
-	}
-
-	var conn *nntp.Conn
-	var err error
-	if s.Server.TLS {
-		conn, err = nntp.DialTLS("tcp", fmt.Sprint(s.Server), s.Server.TLSConfig)
-	} else {
-		conn, err = nntp.Dial("tcp", fmt.Sprint(s.Server))
-	}
+// NewSession creates, authenticates, and attempts to enable compression for a new *Session.
+func NewSession(s *Server, d dialer) (*Session, error) {
+	sn := &Session{Server: s}
+	c, err := d(s)
 	if err != nil {
-		return err
+		return nil, err
 	}
-
-	if err := conn.Authenticate(s.Server.Username, s.Server.password); err != nil {
-		return err
+	sn.c = c
+	if err = sn.c.Authenticate(s.Username, s.password); err != nil {
+		return nil, err
 	}
-
-	s.Connected = true
-	if err := conn.EnableCompression(); err == nil {
-		s.Compressed = true
+	if err = sn.c.EnableCompression(); err == nil {
+		sn.Compressed = true
 	}
-
-	s.conn = conn
-	return nil
+	return sn, nil
 }
 
-// busy marks a session as busy.
-func (s *Session) busy() {
-	s.Server.sessionMutex.Lock()
-	defer s.Server.sessionMutex.Unlock()
-
-	delete(s.Server.idle, s)
-	s.Server.busy[s] = true
-	s.Busy = true
-}
-
-// idle marks a session as idle.
-func (s *Session) idle() {
-	s.Server.sessionMutex.Lock()
-	defer s.Server.sessionMutex.Unlock()
-
-	delete(s.Server.busy, s)
-	s.Server.idle[s] = true
-	s.Busy = false
-}
-
-// close attempts to cleanly QUIT and close the session.
-// Note the connection is always terminated, even if an error is raised on QUIT.
-func (s *Session) close() error {
-	s.Connected = false
-	return s.conn.Quit()
+// Quit() terminates a *Session, including sending QUIT.
+func (s *Session) Quit() error {
+	return s.c.Quit()
 }
 
 // selectGroup switches the session to the requested group.
@@ -95,116 +98,25 @@ func (s *Session) selectGroup(g string) error {
 	if s.CurrentGroup == g {
 		return nil
 	}
-
-	if _, err := s.conn.Group(g); err != nil {
+	if _, err := s.c.Group(g); err != nil {
 		return err
 	}
-
 	s.CurrentGroup = g
 	return nil
 }
 
-// getArticleBody returns an io.Reader for the body of article id (without <>)
-// in the current group.
-func (s *Session) getArticleBody(id string) (io.Reader, error) {
-	body, err := s.conn.Body(util.FormatArticleID(id))
+// writeArticleBody writes the requested article's body to the supplied
+// io.Writer.
+func (s *Session) writeArticleBody(group, id string, w io.Writer) (int64, error) {
+	if err := s.selectGroup(group); err != nil {
+		return 0, err
+	}
+	body, err := s.c.Body(util.FormatArticleID(id))
 	if err != nil {
-		return nil, err
-	}
-	return body, nil
-}
-
-// A Server represents a usenet server, which may have many sessions.
-type Server struct {
-	Hostname        string
-	Port            int
-	TLS             bool
-	TLSConfig       *tls.Config
-	Username        string
-	password        string
-	MaxSessions     int
-	sessionMutex    *sync.Mutex
-	busy            map[*Session]bool
-	idle            map[*Session]bool
-	isDisconnecting bool
-}
-
-// NewServer creates and a initialises a new Server.
-func NewServer(hostname string, port int, useTLS bool, username, password string, maxSessions int) *Server {
-	return &Server{
-		Hostname:     hostname,
-		Port:         port,
-		TLS:          useTLS,
-		TLSConfig:    &tls.Config{InsecureSkipVerify: true},
-		Username:     username,
-		password:     password,
-		MaxSessions:  maxSessions,
-		sessionMutex: &sync.Mutex{},
-		busy:         make(map[*Session]bool),
-		idle:         make(map[*Session]bool),
-	}
-}
-
-// String returns the server as a hostname:port string.
-func (s *Server) String() string {
-	return fmt.Sprintf("%v:%v", s.Hostname, s.Port)
-}
-
-// Disconnect terminates all sessions to the server.
-func (s *Server) Disconnect() {
-	s.isDisconnecting = true
-	defer func() { s.isDisconnecting = false }()
-
-	// TODO(negz): Too broad - i.e. will prevent busy sessions becoming idle
-	s.sessionMutex.Lock()
-	defer s.sessionMutex.Unlock()
-
-	for session := range s.idle {
-		session.close()
-		delete(s.idle, session)
+		return 0, err
 	}
 
-	for session := range s.busy {
-		// TODO(negz): Wait for session to become idle? Will need some timeout.
-		session.close()
-		delete(s.busy, session)
-	}
-}
-
-// newSession returns a new *Session to the server.
-func (s *Server) newSession() (*Session, error) {
-	session := &Session{Server: s}
-	if err := session.dial(); err != nil {
-		return nil, err
-	}
-	return session, nil
-}
-
-// getSession returns a *Session to download... stuff with.
-func (s *Server) getSession() (*Session, error) {
-	if s.isDisconnecting {
-		return nil, SessionError(fmt.Sprintf("server %v is currently disconnecting", s))
-	}
-
-	s.sessionMutex.Lock()
-	defer s.sessionMutex.Unlock()
-
-	// Try to return an idle session
-	for session := range s.idle {
-		return session, nil
-	}
-
-	// We know there's no idle sessions, and we're at max busy sessions.
-	if len(s.busy) == s.MaxSessions {
-		return nil, SessionError(fmt.Sprintf("server %v has no available sessions", s))
-	}
-
-	// We know there's no idle sessions, and we're under max busy sessions.
-	session, err := s.newSession()
-	if err != nil {
-		return nil, err
-	}
-	return session, nil
+	return io.Copy(w, body)
 }
 
 // An ArticleRequest specifies an article to download by its Group and ID
@@ -223,30 +135,8 @@ type ArticleResponse struct {
 	Error error
 }
 
-// writeArticleBody writes the requested article's body to the supplied
-// io.Writer.
-func (s *Server) writeArticleBody(group, id string, w io.Writer) (int64, error) {
-	session, err := s.getSession()
-	if err != nil {
-		return 0, err
-	}
-	session.busy()
-	defer session.idle()
-
-	if err = session.selectGroup(group); err != nil {
-		return 0, err
-	}
-
-	body, err := session.getArticleBody(id)
-	if err != nil {
-		return 0, err
-	}
-
-	return io.Copy(w, body)
-}
-
-// Grabby fulfills ArticleRequests with ArticleResponses.
-func (s *Server) Grabby(req <-chan *ArticleRequest, resp chan<- *ArticleResponse) {
+// Grab fulfills ArticleRequests with ArticleResponses.
+func (s *Session) Grab(req <-chan *ArticleRequest, resp chan<- *ArticleResponse) {
 	for request := range req {
 		bytes, err := s.writeArticleBody(request.Group, request.ID, request.WriteTo)
 		resp <- &ArticleResponse{ArticleRequest: request, Bytes: bytes, Error: err}
